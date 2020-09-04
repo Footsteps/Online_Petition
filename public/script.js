b83d916ec1aa1b65bf2e4dac698bf1ba315b2b8b
Angela Schumacher <angelita.schumacher@gmail.com>
2020-09-04 16:48:15 +0200

    canvas code

---
+++ public/script.js
@@ -0,0 +1,42 @@
+////////////////////////CANVAS & SIGNATURE////////////////////////////////////////////////
+
+//https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener
+console.log("sanity check!");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let signatureD = document.getElementById("signature").value;
+
+ctx.strokeStyle = "royalblue";
+ctx.lineWidth = 3;
+let signing;
+let signature;
+
+canvas.addEventListener("mousedown", down, true);
+canvas.addEventListener("mouseup", up, true);
+
+function down(event) {
+    //console.log("mousesown happened!");
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+    canvas.addEventListener("mousemove", sign, true);
+}
+
+function up(event) {
+    //console.log("mouseup happened!");
+    canvas.removeEventListener("mousemove", sign, true);
+    sign(event);
+    getSignature();
+}
+function sign(event) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+function getSignature() {
+    signature = canvas.toDataURL("image/jpeg", 0.1);
+    //console.log(signature);
+    signatureD = signature;
+    console.log(signatureD);
+    return signatureD;
+}
