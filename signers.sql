35850181fa1ed0304b39e62fb22fc8711e7aaa46
Angela Schumacher <angelita.schumacher@gmail.com>
2020-09-08 13:31:03 +0200

    signers/signatures table

--- signers.sql
+++ signers.sql
@@ -1,30 +1,17 @@
 -- this is how you write a commen in an SQL file
---DROP TABLE IF EXISTS signers;
+--psql -d petition -f signers.sql;
+
+DROP TABLE IF EXISTS signers;
 
 CREATE TABLE signers
 (
     id SERIAL primary key,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
     sign TEXT NOT NULL
+    
 );
 
-INSERT INTO signers
-    (firstname, lastname, sign)
-VALUES
-    ('Angela', 'Schumacher', 'VERY LONG TEXT');
 
-
-INSERT INTO signers
-    (firstname, lastname, sign)
-VALUES
-    ('Brigitte', 'Markmann', 'VERY LONG TEXT');
-
-
-INSERT INTO signers
-    (firstname, lastname, sign)
-VALUES
-    ('Marion', 'Mohr', 'VERY LONG TEXT');
 
 
 
