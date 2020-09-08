edeae6ee2f5fff7acd6545458bea00b748b79237
Angela Schumacher <angelita.schumacher@gmail.com>
2020-09-08 13:27:48 +0200

    login and register added

--- index.js
+++ index.js
@@ -50,7 +50,57 @@ app.use(function (err, req, res, next) {
 ////////////////////////////////ROOT ROUTE //////////////////////////////////////
 app.get("/", (req, res) => {
     //console.log("get request to root route happend!!!");
-    res.redirect("/petition");
+    res.redirect("/register");
+});
+
+//////////////////registration//////////////////////////////////////
+app.get("/register", (req, res) => {
+    console.log("get request to registration route happend!!!");
+    res.render("register", {
+        layout: "main",
+    });
+});
+
+app.post("/register", (req, res) => {
+    console.log("post request to registration route happend!!!");
+    //console.log("req.body: ", req.body);
+    let first = req.body.name;
+    //console.log(firstname);
+    let last = req.body.last;
+    //console.log(lastname);
+    let email = req.body.email;
+    //console.log(sign);
+    let password = req.body.password;
+    //console.log(password);
+    let userID;
+
+    if (first === "" || last === "" || email === "" || password === "") {
+        res.render("register", {
+            error: "Oh, something went wrong! Please try again :) ",
+        });
+    } else {
+        console.log("req.body: ", req.body);
+        //console.log(req.body.password);
+
+        bc.hash(req.body.password).then((salted) => {
+            console.log("salted: ", salted);
+            db.register(req.body.first, req.body.last, req.body.email, salted)
+                .then((id) => {
+                    userId = id.rows[0].id;
+                    //set id as cookie
+                    req.session.userId = userId;
+                    console.log("req.session: ", req.session);
+                    res.redirect("petition");
+                })
+                .catch((err) => {
+                    console.log("err in register: ", err);
+                });
+        });
+    }
+
+    //console.log("req.session: ", req.session);
+
+    //console.log("req.session after adding something: ", req.session);
 });
 
 ////////////////////////////////PETITION ROUTE //////////////////////////////////////
@@ -69,29 +119,27 @@ app.get("/petition", (req, res) => {
 app.post("/petition", (req, res) => {
     console.log("req.body: ", req.body);
     //console.log("req.body: ", req.body);
-    let firstname = req.body.firstname;
-    //console.log(firstname);
-    let lastname = req.body.lastname;
-    //console.log(lastname);
     let sign = req.body.signature;
     //console.log(sign);
-    let userId;
+    let user_id = req.session.userId;
+    let signerId;
 
-    db.addSigner(firstname, lastname, sign)
+    db.addSignature(sign, user_id)
         .then((id) => {
             //console.log(id);
             //console.log(id.rows[0]);#
             //console.log(id.rows[0].id);
             //store id in variable
-            userId = id.rows[0].id;
+            signerId = id.rows[0].id;
             //console.log(thisSign);
             //console.log("req.session: ", req.session);
             //set signature as cookie
-            req.session.id = userId;
+            req.session.id = signerId;
             //console.log("req.session: ", req.session);
-            //set general cookie
+            //set cookie for signing
             req.session.signed = "signed!";
-            console.log("req.session: ", req.session);
+            //console.log("req.session: ", req.session);
+
             res.redirect("/signed");
         })
         .catch((err) => {
@@ -152,11 +200,72 @@ app.get("/signers", (req, res) => {
             res.render("signers", {
                 rows: rows,
             });
-            //console.log("data: ", rows);
+            console.log("data: ", rows);
         })
         .catch((err) => {
             console.log("err in getSigners: ", err);
         });
+});
+
+//////////////////////////////login////////////////////////////////////////
+app.get("/login", (req, res) => {
+    console.log("get request to login route happend!!!");
+    res.render("login", {
+        layout: "main",
+    });
+});
+
+app.post("/login", (req, res) => {
+    console.log("post request to login route happend!!!");
+    //console.log("req.body: ", req.body);
+    let emailLogin = req.body.email;
+    //console.log(sign);
+    let passwordLogin = req.body.password;
+    //console.log(password);
+    //console.log("req.body: ", req.body);
+    let hash;
+
+    if (emailLogin === "" || passwordLogin === "") {
+        res.render("login", {
+            error: "Oh, something went wrong! Please try again :) ",
+        });
+    } else {
+        db.email(req.body.email)
+            .then(({ rows }) => {
+                //console.log("rows: ", rows[0].email);
+                //console.log(rows[0].password);
+                //console.log(passwordLogin);
+                //console.log(rows[0].id);
+                hash = rows[0].password;
+
+                bc.compare(passwordLogin, rows[0].password).then((result) => {
+                    if (result == true) {
+                        console.log("password works!!!!");
+                        req.session.userId = rows[0].id;
+                        //console.log("req.session: ", req.session);
+                        if (!req.session.signed) {
+                            res.render("petition", {
+                                layout: "main",
+                            });
+                        } else {
+                            res.redirect("/signed");
+                        } //closes if-else-cookie
+                    } else {
+                        res.render("login", {
+                            error:
+                                "Oh, something went wrong! Please try again :) ",
+                        });
+                    }
+                });
+            })
+            .catch((err) => {
+                console.log("err in email: ", err);
+            });
+    }
+
+    //console.log("req.session: ", req.session);
+
+    //console.log("req.session after adding something: ", req.session);
 });
 
 /////////////////////////////////protecting from iframe///////////////////////////////
@@ -164,50 +273,6 @@ app.get("/signers", (req, res) => {
 app.use(function (req, res, next) {
     res.setHeader("x-frame-options", "deny");
     next();
-});
-
-//////////////////registration//////////////////////////////////////
-app.get("/register", (req, res) => {
-    console.log("get request to registration route happend!!!");
-    res.render("register", {
-        layout: "main",
-    });
-});
-
-app.post("/register", (req, res) => {
-    console.log("post request to registration route happend!!!");
-    //console.log("req.body: ", req.body);
-    let first = req.body.name;
-    //console.log(firstname);
-    let last = req.body.last;
-    //console.log(lastname);
-    let email = req.body.email;
-    //console.log(sign);
-    let password = req.body.password;
-
-    if (first === "" || last === "" || email === "" || password === "") {
-        res.render("register", {
-            error: "Oh, something went wrong! Please try again :) ",
-        });
-    } else {
-        console.log("req.body: ", req.body);
-        //console.log(req.body.password);
-
-        bc.hash(req.body.password).then((salted) => {
-            console.log("salted: ", salted);
-            db.register(req.body.name, req.body.last, req.body.email, salted)
-                .then(({ rows }) => {
-                    console.log("rows: ", rows);
-                })
-                .catch((err) => {
-                    console.log("err in register: ", err);
-                });
-        });
-    }
-
-    //console.log("req.session: ", req.session);
-
-    //console.log("req.session after adding something: ", req.session);
 });
 
 app.listen(8080, () => console.log("petition server is running :)"));
