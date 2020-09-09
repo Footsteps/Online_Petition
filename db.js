04c2732fa8ae2858c2b256ecc89d76d6e28a0da7
Angela Schumacher <angelita.schumacher@gmail.com>
2020-09-09 11:26:49 +0200

    database code

--- db.js
+++ db.js
@@ -13,6 +13,10 @@ module.exports.getTableUsers = () => {
     return db.query(`SELECT * FROM users`);
 };
 
+module.exports.getTableProfiles = () => {
+    return db.query(`SELECT * FROM user_profiles`);
+};
+
 module.exports.addSignature = (sign, user_id) => {
     return db.query(
         `
@@ -25,11 +29,17 @@ module.exports.addSignature = (sign, user_id) => {
 };
 
 module.exports.getSigners = () => {
-    return db.query(`SELECT first, last FROM users`);
+    return db.query(`SELECT 
+    users.first, users.last, user_profiles.age, user_profiles.city, user_profiles.url
+FROM signers 
+LEFT JOIN users
+ON signers.user_id = users.id
+JOIN user_profiles
+ON signers.user_id = user_profiles.user_id;`);
 };
 
 module.exports.getSignature = (id) => {
-    return db.query(`SELECT sign FROM signers WHERE ${id} = id;`);
+    return db.query(`SELECT sign FROM signers WHERE id = $1`, [id]);
 };
 
 ////////////////registration////////////////////////////////
@@ -54,4 +64,31 @@ module.exports.email = (email) => {
     return db.query(`SELECT * FROM users WHERE email = $1`, [email]);
 };
 
-/////////////////get password/////////////////////////
+///////////////////profile/////////////////////////////////////
+module.exports.profiling = (age, city, url, user_id) => {
+    return db.query(
+        `
+    INSERT INTO user_profiles (age, city, url, user_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id
+    `,
+        [age || null, city || null, url || null, user_id]
+    );
+};
+
+////////////////////////cities//////////////////////////
+
+module.exports.getCities = (city) => {
+    return db.query(
+        `SELECT 
+    users.first, users.last, user_profiles.city
+    FROM signers 
+    LEFT JOIN users
+    ON signers.user_id = users.id
+    JOIN user_profiles
+    ON signers.user_id = user_profiles.user_id
+    WHERE LOWER(user_profiles.city) = LOWER($1)
+    `,
+        [city]
+    );
+};
