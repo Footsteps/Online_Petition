5da1c3c3b14b44ac8cc24f030f3b8b00fcb7894e
Angela Schumacher <angelita.schumacher@gmail.com>
2020-09-09 11:27:38 +0200

    table for creating the profile

---
+++ user_profiles.sql
@@ -0,0 +1,10 @@
+--psql -d petition -f user_profiles.sql;
+
+DROP TABLE IF EXISTS user_profiles CASCADE;
+CREATE TABLE user_profiles(
+  id SERIAL PRIMARY KEY,
+  age INT,
+    city VARCHAR(255),
+  url VARCHAR(255),
+  user_id INT NOT NULL REFERENCES users(id)
+);
